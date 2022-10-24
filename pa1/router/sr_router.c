a3c83a190312f0b9ac0650da11e95ccff8b4c1c1
Sherman Liu <sherman.liu@mail.utoronto.ca>
2022-10-24 00:13:04 -0400

    reverted handle_ar_req forward packet strat

--- pa1/router/sr_router.c
+++ pa1/router/sr_router.c
@@ -198,8 +198,7 @@ void handle_arp_req(struct sr_instance *sr, uint8_t *packet, unsigned int length
   print_hdr_eth((uint8_t *)new_ethr_hdr);
   print_hdr_arp((uint8_t *)new_arp_rep_hdr);
 
-  forward_packet(sr, new_hdr, length, sr_if, old_arp_hdr->ar_sip);
-  /*sr_send_packet(sr, new_hdr, length, interface);*/
+  sr_send_packet(sr, new_hdr, length, interface);
   free(new_hdr);
 }
 
